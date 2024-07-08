8c60f8a91394abeb3a32f8c8e35d8c2bd33053ea
D1204463 <161418099+D1204463@users.noreply.github.com>
2024-07-09 01:30:21 +0800

    計算機app完成

--- calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,43 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void add(View view){
+        EditText num1View = (EditText)findViewById(R.id.num1);
+        EditText num2View = (EditText)findViewById(R.id.num2);
+        TextView resultView = (TextView) findViewById(R.id.result);
+
+        Double addResult = Double.parseDouble(num1View.getText().toString()) + Double.parseDouble(num2View.getText().toString());
+        resultView.setText(String.valueOf(addResult));
+    }
+    public void cut(View view){
+        EditText num1View = (EditText) findViewById(R.id.num1);
+        EditText num2View = (EditText)findViewById(R.id.num2);
+        TextView resultView = (TextView) findViewById(R.id.result);
+
+        Double cutResult = Double.parseDouble(num1View.getText().toString()) - Double.parseDouble(num2View.getText().toString());
+        resultView.setText(String.valueOf(cutResult));
+
+    }
+    public void multiply(View view){
+        EditText num1View = (EditText) findViewById(R.id.num1);
+        EditText num2View = (EditText)findViewById(R.id.num2);
+        TextView resultView = (TextView) findViewById(R.id.result);
+
+        Double multiplyResult = Double.parseDouble(num1View.getText().toString()) * Double.parseDouble(num2View.getText().toString());
+        resultView.setText(String.valueOf(multiplyResult));
+
+    }
+    public void divide(View view){
+        EditText num1View = (EditText) findViewById(R.id.num1);
+        EditText num2View = (EditText)findViewById(R.id.num2);
+        TextView resultView = (TextView) findViewById(R.id.result);
+        if( Double.parseDouble(num2View.getText().toString()) == 0){
+            resultView.setText("數字不可為0");
+        }
+        else{
+            double divideResult = Double.parseDouble(num1View.getText().toString()) / Double.parseDouble(num2View.getText().toString());
+            resultView.setText(String.valueOf(divideResult));
+        }
     }
 }
